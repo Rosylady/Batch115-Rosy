14a08473ea76992e3d763f1ec8132669a8e66dfa
HP <HP@DESKTOP-F5LDE40>
2024-09-02 11:17:35 +0630

    Version2

---
+++ src/home/com/StudentTesting.java
@@ -0,0 +1,11 @@
+package home.com;
+
+public class StudentTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to my world");
+
+	}
+
+}
